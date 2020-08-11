796f135174b5c5a8dcca84341918db3a2fe9463d
Roopali Bhadoria <Roopali.Bhadoria@unilogcorp.com>
2020-08-11 20:52:00 +0530

    Updates

---
+++ precache-manifest.b749e19a11fe17ee6c45dcba5a718aac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a18a34178273f2e0e41f5693d7796acf",
+    "url": "/ReactRouting/index.html"
+  },
+  {
+    "revision": "2032d19da4d90aee4673",
+    "url": "/ReactRouting/static/css/main.a47849ef.chunk.css"
+  },
+  {
+    "revision": "9f76b607f1f619dcf0c3",
+    "url": "/ReactRouting/static/js/2.3cc163c4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactRouting/static/js/2.3cc163c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2032d19da4d90aee4673",
+    "url": "/ReactRouting/static/js/main.d228c533.chunk.js"
+  },
+  {
+    "revision": "7b79d8926bcf2876d0a6",
+    "url": "/ReactRouting/static/js/runtime-main.bc460599.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactRouting/static/media/logo.5d5d9eef.svg"
+  }
+]);
